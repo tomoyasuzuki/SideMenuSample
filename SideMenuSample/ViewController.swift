9e958142ba5b130c38ecb3e61caae82c76e809f5
tomoyasuzuki <tomoya.s0121@icloud.com>
2019-07-15 17:19:04 +0900

    Initial Commit

---
+++ SideMenuSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenuSample
+//
+//  Created by 鈴木友也 on 2019/07/15.
+//  Copyright © 2019 tomoya.suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
